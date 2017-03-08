03f78d8e78c48ca6a92f0a523f536ba8749739ca
abhijithvk191 <abijithvjn@gmail.com>
2017-03-08 17:47:05 +0530

    Update Recursion-multi_list_to_single.py

--- Recursion-multi_list_to_single.py
+++ Recursion-multi_list_to_single.py
@@ -1,12 +1,21 @@
 list1 = [1,2,[3,4,[5,6,7]],8,[9,[10,[11,12]]]]
 result=[]
 def type_fun(x):
-  for items in x:
-    if isinstance(items,list):
-      if type_fun(items):
-        return True
-      else:  
-        result.append(items)
-    else:
-      result.append(items)
-type_fun(list1)
+  if isinstance(x,list):
+    
+    return True
+for items in list1:
+  if type_fun(items):
+    for items1 in items:
+      if type_fun(items1):
+        for items2 in items1:
+          if type_fun(items2):
+            for item3 in items2:
+              result.append(item3)
+          else:
+              result.append(items2)
+      else:
+        result.append(items1)
+  else:
+    result.append(items)
+print(result)
